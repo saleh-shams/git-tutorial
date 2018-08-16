3d0d4e0b5ee9f5567452ea9bffa80a39c6aa1380
saleh <mdsalehraj@gmail.com>
2018-08-16 23:28:17 +0600

    another comment

--- hello.java
+++ hello.java
@@ -8,5 +8,25 @@ public class Hello {
     public static void greeting (string name){
         System.out.println("hi" + name);
     }
+
+    return greeting (string name){
+        System.out.println("hi" + name);
+    }
+}
+
+public class Hello {
+    public static void main(string[] args){
+        System.out.println("VS code is Awesome.");
+
+        greeting ("change");
+    }
+
+    public static void greeting (string name){
+        System.out.println("hi" + name);
+    }
+
+    return greeting (string name){
+        System.out.println("hi" + name);
+    }
 }
 
