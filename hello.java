ac965b8d45b17e5f574b053a0c922ffca44a4026
saleh <mdsalehraj@gmail.com>
2018-08-16 21:32:44 +0600

    changing method

--- hello.java
+++ hello.java
@@ -2,11 +2,11 @@ public class Hello {
     public static void main(string[] args){
         System.out.println("VS code is Awesome.");
 
-        greeting ("Saleh");
+        greeting ("change");
     }
 
     public static void greeting (string name){
-        System.out.println("welcome" + name);
+        System.out.println("hi" + name);
     }
 }
 
