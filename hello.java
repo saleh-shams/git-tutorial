684988f688b45c994565adc5a883384910aacaa9
saleh <mdsalehraj@gmail.com>
2018-08-16 18:21:38 +0600

    added another method on first.java

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(string[] args){
-        System.out.println("VS code is Awesome.")
+        System.out.println("VS code is Awesome.");
+
+        greeting ("Saleh");
+    }
+
+    public static void greeting (string name){
+        System.out.println("welcome" + name);
     }
 }
+
